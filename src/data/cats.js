06c401d76aaa73ed1dd6bc5e58d850924a45a4a8
Robert Kiemele <robert.kiemele@bexio.com>
2020-05-13 16:37:59 +0200

    Added dummy data

---
+++ src/data/cats.js
@@ -0,0 +1,32 @@
+export default [
+  {
+    name: 'Fish',
+    breed: 'tuxedo',
+    gender: 'male',
+    age: 20,
+    color: 'black/white',
+    weight: 13,
+    location: 'fourside',
+    notes: 'Sweet kitty. He loves getting his belly rubbed.'
+  },
+  {
+    name: 'Henry',
+    breed: 'tabby',
+    gender: 'male',
+    age: 20,
+    color: 'white',
+    weight: 17,
+    location: 'threed',
+    notes: 'Super chilled cat.'
+  },
+  {
+    name: 'Roger',
+    breed: 'tabby',
+    gender: 'male',
+    age: 11,
+    color: 'black',
+    weight: 15,
+    location: 'threed',
+    notes: 'Very good with kids.'
+  }
+]
