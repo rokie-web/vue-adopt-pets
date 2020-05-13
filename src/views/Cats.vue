00ca762f1a89a7135b94b20ecd6c75b5a22d51ba
Robert Kiemele <robert.kiemele@bexio.com>
2020-05-13 16:38:47 +0200

    Added table to views

--- src/views/Cats.vue
+++ src/views/Cats.vue
@@ -1,14 +1,16 @@
 <template>
   <div>
-     <b-table striped hover :items="items"></b-table>
+    <h1>Cats for Adoption</h1>
+    <b-table striped hover :items="items"></b-table>
   </div>
 </template>
 
 <script>
+import cats from '@/data/cats'
 export default {
   data () {
     return {
-
+      items: cats
     }
   }
 }
--- src/views/Dogs.vue
+++ src/views/Dogs.vue
@@ -1,3 +1,17 @@
 <template>
-  <h1>Dogs</h1>
+  <div>
+    <h1>Dogs for Adoption</h1>
+    <b-table striped hover :items="items"></b-table>
+  </div>
 </template>
+
+<script>
+import dogs from '@/data/dogs'
+export default {
+  data () {
+    return {
+      items: dogs
+    }
+  }
+}
+</script>
